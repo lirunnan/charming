f84d81f0d7738a0b9ad5cff79a8c5bcff0603b08
godrogers.github.io/lrn.github.io/ <15910288422@163.com>
2017-05-02 18:17:18 +0800

    blog

--- blog/js/style.js
+++ blog/js/style.js
@@ -15,21 +15,33 @@ function popClose() {
 $(document).scroll(function() {
     $(".back").show();
 })
-window.onload = function() {
-    setInterval(function() {
-        var date = new Date();
-        var year = date.getFullYear(); //获取当前年份
-        var mon = date.getMonth() + 1; //获取当前月份
-        var da = date.getDate(); //获取当前日
-        var day = date.getDay(); //获取当前星期几
-        var h = date.getHours(); //获取小时
-        var m = date.getMinutes(); //获取分钟
-        var s = date.getSeconds(); //获取秒
-        var d = document.getElementById('time');
-        d.innerHTML = year + '年' + mon + '月' + da + '日' + "," + '星期' + day + ',' + h + ':' + m + ':' + s;
-    }, 1000)
+if (document.getElementById("time")) {
+    window.onload = function() {
+        setInterval(function() {
+            var date = new Date();
+            var year = date.getFullYear(); //获取当前年份
+            var mon = date.getMonth() + 1; //获取当前月份
+            var da = date.getDate(); //获取当前日
+            var day = date.getDay(); //获取当前星期几
+            var h = date.getHours(); //获取小时
+            var m = date.getMinutes(); //获取分钟
+            var s = date.getSeconds(); //获取秒
+            var d = document.getElementById('time');
+            d.innerHTML = year + '年' + mon + '月' + da + '日' + "," + '星期' + day + ',' + h + ':' + m + ':' + s;
+        }, 1000)
+    }
 }
-
+var div = document.getElementsByTagName("div"),
+    num = 0,
+    j = setInterval(function() {
+        if (num < div.length) {
+            div[num].style.opacity = 1;
+            num++;
+        } else {
+            clearInterval(j);
+        }
+        console.log(num);
+    }, 300)
 //禁止右键和f12
 // function click(e) {
 //     if (document.all) {
