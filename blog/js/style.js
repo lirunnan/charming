8f3bd12bf817e68d12d83a927392098eda2e5002
godrogers.github.io/lrn.github.io/ <15910288422@163.com>
2017-05-31 17:51:41 +0800

    hi

--- blog/js/style.js
+++ blog/js/style.js
@@ -44,28 +44,28 @@ var div = document.getElementsByTagName("div"),
     }
   }, 200);
 
-function click(e) {
-  if (document.all) {
-    if (event.button == 2 || event.button == 3) {
-      oncontextmenu = 'return false';
-      alert("你要干嘛");
-    }
-  }
-  if (document.layers) {
-    if (e.which == 3) {
-      oncontextmenu = 'return false';
-      alert("你要干嘛");
-    }
-  }
-}
-if (document.layers) {
-  document.captureEvents(Event.MOUSEDOWN);
-}
-document.onmousedown = click;
-document.oncontextmenu = new Function("return false;")
-document.onkeydown = document.onkeyup = document.onkeypress = function() {
-  if (window.event.keyCode == 123) {
-    window.event.returnValue = false;
-    return (false);
-  }
-}
+// function click(e) {
+//   if (document.all) {
+//     if (event.button == 2 || event.button == 3) {
+//       oncontextmenu = 'return false';
+//       alert("你要干嘛");
+//     }
+//   }
+//   if (document.layers) {
+//     if (e.which == 3) {
+//       oncontextmenu = 'return false';
+//       alert("你要干嘛");
+//     }
+//   }
+// }
+// if (document.layers) {
+//   document.captureEvents(Event.MOUSEDOWN);
+// }
+// document.onmousedown = click;
+// document.oncontextmenu = new Function("return false;")
+// document.onkeydown = document.onkeyup = document.onkeypress = function() {
+//   if (window.event.keyCode == 123) {
+//     window.event.returnValue = false;
+//     return (false);
+//   }
+// }
