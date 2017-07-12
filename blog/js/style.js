4b504136b7c30fe7ada359636e440015ba202aa9
ubuntu <15910288422@163.com>
2017-07-12 18:31:21 +0800

    exp

--- blog/js/style.js
+++ blog/js/style.js
@@ -59,6 +59,26 @@ function popClose() {
 $("#money").click(function() {
   ga('send', 'event', 'money', '打赏');
 })
+// 冒泡子
+function maopao(arra) {
+  var temp;
+  for (var i = 0; i < arra.length; i++) { //比较多少趟，从第一趟开始
+    for (var j = 0; j < arra.length - i - 1; j++) { //每一趟比较多少次数
+      if (arra[j] > arra[j + 1]) {
+        temp = arra[j];
+        arra[j] = arra[j + 1];
+        arra[j + 1] = temp;
+      }
+    }
+  };
+  return arra;
+}
+
+var arrry = [85, 24, 63, 17, 31, 17, 86, 50];
+
+var s = maopao(arrry);
+
+console.log(s)
 app.controller("homeCtrl", function() {
 
 });
